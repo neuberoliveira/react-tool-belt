65090fb1e376cc3317f69f14128413cbc0aecec9
Neuber Oliveira <neuber.oliveira05@gmail.com>
2017-05-26 16:10:02 -0300

    learning...

---
+++ reverse/reverse.py
@@ -0,0 +1,9 @@
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(prog='Reverse', description='Call adb reverse tcp:8081 tcp:8081')
+args = parser.parse_args()
+
+print args
+#subprocess.call(["adb", "reverse", "tcp:8081", "tcp:8081"]);
+print 'done'
