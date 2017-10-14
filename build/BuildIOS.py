4818f2c805a0a853009f0253b9f0dc678a5dd64c
Neuber Oliveira <neuber.oliveira05@gmail.com>
2017-10-14 15:39:08 -0300

    for some reason the archived file name is different between my work machine and home machine
    Work Machine: DAY-MONTH-YEAR the year with just last 2 digits
    Home Machine: DAY-MONTH-YEAR the yeear here is full with 4 digits

--- build/BuildIOS.py
+++ build/BuildIOS.py
@@ -56,6 +56,12 @@ class BuildIOS(BuildInterface):
         
         try:
             scriptdir = os.path.dirname(os.path.realpath(__file__))
+            
+            #archive
+            print 'Archive project'
+            archivecode = subprocess.call(["xcodebuild", "-scheme", self.appName, "archive"], cwd="ios")
+            
+            #format dates
             date = datetime.datetime.now()
             separator = "-"
             date_year = str(date.year)
@@ -63,15 +69,11 @@ class BuildIOS(BuildInterface):
             date_day = str(date.day).rjust(2, '0')
             
             todaydir = date_year+separator+date_month+separator+date_day
-            todayfile = date_day+separator+date_month+separator+date_year[2:]
-            
-            #archive
-            print 'Archive project'
-            archivecode = subprocess.call(["xcodebuild", "-scheme", self.appName, "archive"], cwd="ios")
-            
+            todayfileYearFull = date_day+separator+date_month+separator+date_year
+            todayfileYear = date_day+separator+date_month+separator+date_year[2:]
+
             archivepath = os.getenv("HOME")+"/Library/Developer/Xcode/Archives/"+todaydir+"/"
-            pattern = archivepath+self.appName+" "+todayfile+"*"
-            archives = glob.glob(pattern)
+            archives = glob.glob(archivepath+self.appName+" "+todayfileYear+"*") + glob.glob(archivepath+self.appName+" "+todayfileYearFull+"*")
             
             if archivecode==0 and len(archives)>0:
                 archivefile = archives[-1]
